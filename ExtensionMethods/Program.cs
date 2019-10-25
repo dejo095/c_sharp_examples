80dc00529d0cc67c8a3f56168680dc98bc27e87e
Dejan <Dejan@DESKTOP-9430TLJ>
2019-10-25 14:53:00 +0200

    initial commit

---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string post = "blah something else then I spoke to somebody else and blah blah";
+
+            var shortenedPost = post.Shorten(5);
+
+            Console.WriteLine(shortenedPost);
+        }
+
+    }
+}
---
+++ ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace ExtensionMethods
+{
+    public static class StringExtensions
+    {
+        public static string Shorten(this String str, int NumberOfWords)
+        {
+            if (NumberOfWords < 0)
+                throw new ArgumentOutOfRangeException("number should be greater then 0");
+
+            if (NumberOfWords == 0)
+            {
+                return "";
+            }
+
+            var words = str.Split(' ');
+
+            if (words.Length <= NumberOfWords)
+            {
+                return str;
+            }
+
+            return string.Join(" ", words.Take(NumberOfWords)) + "...";
+            
+        }
+    }
+}
